d473367096d30ac3ab0cfacf2bb1b793cf82a13b
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2023-11-16 21:41:48 +0530

    Update modifying_tables.sql

--- modifying_tables.sql
+++ modifying_tables.sql
@@ -9,4 +9,21 @@ ADD COLUMN coffee_origin VARCHAR(30);
 ALTER TABLE products
 DROP COLUMN coffee_origin;
 
--- Creating a dummy table for deleting it
+-- Creating a dummy database and table for deleting the table
+CREATE DATABASE example;
+USE example;
+CREATE TABLE dummy(
+id INT auto_increment PRIMARY KEY,
+name VARCHAR(30),
+price DECIMAL(3,2)
+);
+
+DESCRIBE dummy;
+SHOW TABLES;
+
+DROP Table dummy;
+
+INSERT INTO dummy (name,price) VALUES('Sakshi',2.54),('Simran',3.54),('Abhishek',4.54);
+SELECT * FROM dummy;
+
+TRUNCATE TABLE dummy;
